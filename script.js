d870c8161d137125795e961d6674ae4b7dc1fd8a
Aaron Platte <aar2latte@gmail.com>
2024-05-16 10:20:28 -0400

    fixed hamburger menu

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-// JS File
+// function toggleMenu() {
+//     const navLinks = document.querySelector('.nav-links');
+//     navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+// }
